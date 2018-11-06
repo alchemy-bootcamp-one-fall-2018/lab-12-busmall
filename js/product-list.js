22d947f2011c0591b346296b2102fdd451316be2
Shaba9 <shaba92@gmail.com>
2018-11-06 14:24:29 -0800

    created objects out of images

---
+++ js/product-list.js
@@ -0,0 +1,84 @@
+const productList = [
+    {
+        'name': 'Bag',
+        'image_url': '../assets/bag.jpg'
+    },
+    {
+        'name': 'Banana',
+        'image_url': '../assets/banana.jpg'
+    },
+    {
+        'name': 'Bathroom',
+        'image_url': '../assets/bathroom.jpg'
+    },
+    {
+        name: 'Boots',
+        image_url: '../assets/boots.jpg'
+    },
+    {
+        name: 'Breakfast',
+        image_url: '../assets/breakfast.jpg'
+    },
+    {
+        name: 'Bubblegum',
+        image_url: '../assets/bubblegum.jpg'
+    },
+    {
+        name: 'Chair',
+        image_url: '../assets/chair.jpg'
+    },
+    {
+        name: 'Cthulhu',
+        image_url: '../assets/cthulhu.jpg'
+    },
+    {
+        name: 'Dog-Duck',
+        image_url: '../assets/dog-duck.jpg'
+    },
+    {
+        name: 'Dragon',
+        image_url: '../assets/dragon.jpg'
+    },
+    {
+        name: 'Pen',
+        image_url: '../assets/pen.jpg'
+    },
+    {
+        name: 'Pet Sweep',
+        image_url: '../assets/pet-sweep.jpg'
+    },
+    {
+        name: 'Scissors',
+        image_url: '../assets/scissors.jpg'
+    },
+    {
+        name: 'Shark',
+        image_url: '../assets/shark.jpg'
+    },
+    {
+        name: 'Sweep',
+        image_url: '../assets/sweep.png'
+    },
+    {
+        name: 'Tauntaun',
+        image_url: '../assets/tauntaun.jpg'
+    },
+    {
+        name: 'Unicorn',
+        image_url: '../assets/unicorn.jpg'
+    },
+    {
+        name: 'USB',
+        image_url: '../assets/usb.gif'
+    },
+    {
+        name: 'Water Can',
+        image_url: '../assets/water-can.jpg'
+    },
+    {
+        name: 'Wine Glass',
+        image_url: '../assets/wine-glass.jpg'
+    }
+];
+
+export default productList;
