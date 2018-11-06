37126bfdf4cd3fecc117122c02221c6c01c9ec33
Tyler Corbett <thetylercorbett@gmail.com>
2018-11-06 15:26:14 -0800

    created raw product data

---
+++ JS/products-data.js
@@ -0,0 +1,86 @@
+// eslint-disable-next-line no-unused-vars
+const productData = [
+    {
+        name: 'breakfast',
+        image: '../assets/breakfast.jpg'
+    },
+    {
+        name: 'bubblegum',
+        image: '../assets/bubblegum.jpg'
+    },
+    {
+        name: 'chair',
+        image: '../assets/chair.jpg'
+    },
+    {
+        name: 'cthulhu',
+        image: '../assets/cthulhu.jpg'  
+    },
+    {
+        name: 'bag',
+        image: '../assets.bag.jpg'
+    },
+    {
+        name: 'banana',
+        image: '../assets.banana.jpg'
+    },
+    {
+        name: 'bathroom',
+        image: '../assets.bathroom.jpg'
+    },
+    {
+        name: 'boots',
+        image: '../assets.boots.jpg'
+    },
+    {
+        name: 'dog-duck',
+        image: '../assets/dog-duck.jpg'
+    },
+    {
+        name: 'dragon',
+        image: '../assets/dragon.jpg'
+    },
+    {
+        name: 'pen',
+        image: '../assets/pen.jpg'
+    },
+    
+    {
+        name: 'pet-sweep',
+        image: '../assets/pet-sweep.jpg'
+    },
+    {
+        name: 'unicorn-meat',
+        image: '../assets/unicorn.jpg'
+    },
+    {
+        name: 'usb',
+        image: '../assets/usb.gif'
+    },
+    {
+        name: 'water-can',
+        image: '../assets/water-can.jpg'
+    },
+    {
+        name: 'wine-glass',
+        image: '../assets/wine-glass'
+    },
+    {
+        name: 'scissors',
+        image: '../assets/scissors.jpg'
+    },
+    {
+        name: 'shark',
+        image: '../assets/shark.jpg'
+    },
+    {
+        name: 'sweep',
+        image: '../assets/sweep.jpg'
+    },
+    {
+        name: 'tauntaun',
+        image: '../assets/tauntaun.jpg'
+    },
+];
+
+export default productData;
