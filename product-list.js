5ad13d52bfb74149ce11da4c5f1e39e7ae393811
Kristin Hortsch <kristinhortsch@gmail.com>
2018-11-06 14:24:11 -0800

    product-list.js finished

--- product-list.js
+++ product-list.js
@@ -1,39 +1,84 @@
-let products = {
+let products = [
     {
         name: 'R2-D2 Suitcase',
-        image: 'bag.png'
+        image: './assets/bag.jpg'
     },
     {
         name: 'Banana Slicer',
-        image: 'banana.png'
+        image: './assets/banana.jpg'
     },
     {
         name: 'iPad Toilet Paper Holder Stand',
-        image: 'bathroom.png'
+        image: './assets/bathroom.jpg'
     },
     {
         name: 'Toeless Rain Boots',
-        image: 'boots.png'
+        image: './assets/boots.jpg'
     }, 
     {
-        name: 'product name',
-        image: 'product.png'
+        name: 'All in 1 Breakfast Maker',
+        image: './assets/breakfast.jpg'
     }, 
     {
-        name: 'product name',
-        image: 'product.png'
+        name: 'Meatball Bubble Gum',
+        image: './assets/bubblegum.jpg'
     },
     {
-        name: 'product name',
-        image: 'product.png'
+        name: 'Convex Chair',
+        image: './assets/chair.jpg'
     },
     {
-        name: 'product name',
-        image: 'product.png'
+        name: 'Cthulhu Action Figure',
+        image: './assets/cthulhu.jpg'
     }, 
     {
-        name: 'product name',
-        image: 'product.png'
+        name: 'Dog Duck Mask',
+        image: './assets/dog-duck.jpg'
     }, 
+    {
+        name: 'Dragon Meat',
+        image: './assets/dragon.jpg'
+    }, 
+    {
+        name: 'Utensil Pen Topper',
+        image: './assets/pen.jpg'
+    }, 
+    {
+        name: 'Pet Sweeper Dust Boots',
+        image: './assets/pet-sweet.jpg'
+    }, 
+    {
+        name: 'Pizza Scissors',
+        image: './assets/scissors.jpg'
+    }, 
+    {
+        name: 'Shark Sleeping Bag',
+        image: './assets/shark.jpg'
+    }, 
+    {
+        name: 'Baby Sweeper Onesie',
+        image: './assets/sweep.png'
+    }, 
+    {
+        name: 'Tauntaun Sleeping Bag',
+        image: './assets/tauntaun.jpg'
+    }, 
+    {
+        name: 'Unicorn Meat',
+        image: './assets/unicorn.jpg'
+    }, 
+    {
+        name: 'Lizard Tail USB',
+        image: './assets/usb.gif'
+    },
+    {
+        name: 'Doomed to Fail Watering Can',
+        image: './assets/water-can.jpg'
+    },
+    {
+        name: 'What Is Life Wine Glass',
+        image: './assets/wine-glass.jpg'
+    }
+];
 
-}
+export default products;
