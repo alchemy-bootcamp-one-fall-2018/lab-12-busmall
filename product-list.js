284f375819732bed20a82a8ed423d5ece3e41693
Kristin Hortsch <kristinhortsch@gmail.com>
2018-11-06 14:10:35 -0800

    file set up

---
+++ product-list.js
@@ -0,0 +1,39 @@
+let products = {
+    {
+        name: 'R2-D2 Suitcase',
+        image: 'bag.png'
+    },
+    {
+        name: 'Banana Slicer',
+        image: 'banana.png'
+    },
+    {
+        name: 'iPad Toilet Paper Holder Stand',
+        image: 'bathroom.png'
+    },
+    {
+        name: 'Toeless Rain Boots',
+        image: 'boots.png'
+    }, 
+    {
+        name: 'product name',
+        image: 'product.png'
+    }, 
+    {
+        name: 'product name',
+        image: 'product.png'
+    },
+    {
+        name: 'product name',
+        image: 'product.png'
+    },
+    {
+        name: 'product name',
+        image: 'product.png'
+    }, 
+    {
+        name: 'product name',
+        image: 'product.png'
+    }, 
+
+}
