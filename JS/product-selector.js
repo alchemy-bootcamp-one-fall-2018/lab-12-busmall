c9dfde2b1c95cc8aab2575fa3e226401b816e40e
Tyler Corbett <thetylercorbett@gmail.com>
2018-11-06 17:42:52 -0800

    can grab 3 random items from the product list. variable name is display

--- JS/product-selector.js
+++ JS/product-selector.js
@@ -6,3 +6,14 @@ function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
 }
 
+
+let display = [];
+
+while(display.length < 3) {
+    let index = getRandomInt(products.length); 
+    if(display.includes(products[index]) === false) {
+        display.push(products[index]);
+    } 
+}
+
+console.log(display);
