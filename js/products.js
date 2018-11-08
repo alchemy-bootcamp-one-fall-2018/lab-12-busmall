57dca3bb19c04772edf7041719f34eb62185528d
Lise Brackbill <menagerie@hevanet.com>
2018-11-07 22:54:10 -0800

    more attempts to get the counter to increment in local storage

--- js/products.js
+++ js/products.js
@@ -2,120 +2,100 @@ export default [
     {
         name: 'bag',
         image: '../bag.jpg',
-        voted: '0',
     },
 
     {
         name: 'banana',
         image: '../banana.jpg',
-        voted: '0',
     },
 
     {
         name: 'bathroom',
         image: '../bathroom.jpg',
-        voted: '0',
     },
 
     {
         name: 'boots',
         image: '../boots.jpg',
-        voted: '0',
     },
 
     {
         name: 'breakfast',
         image: '../breakfast.jpg',
-        voted: '0',
     },
 
     {
         name: 'bubblegum',
         image: '../bubblegum.jpg',
-        voted: '0',
     },
 
     {
         name: 'chair',
         image: '../chair.jpg',
-        voted: '0',
     },
 
     {
         name: 'cthulhu',
         image: '../cthulhu.jpg',
-        voted: '0',
     },
 
     {
         name: 'dog-duck',
         image: '../dog-duck.jpg',
-        voted: '0',
     },
 
     {
         name: 'dragon',
         image: '../dragon.jpg',
-        voted: '0',
     },
 
     {
         name: 'pen',
         image: '../pen.jpg',
-        voted: '0',
     },
 
     {
         name: 'pet-sweep',
         image: '../pet-sweep.jpg',
-        voted: '0',
     },
 
     {
         name: 'scissors',
         image: '../scissors.jpg',
-        voted: '0',
     },
 
     {
         name: 'shark',
         image: '../shark.jpg',
-        voted: '0',
     },
 
     {
         name: 'sweep',
         image: '../sweep.png',
-        voted: '0',
     },
 
     {
         name: 'tauntaun',
         image: '../tauntaun.jpg',
-        voted: '0',
     },
 
     {
         name: 'unicorn',
         image: '../unicorn.jpg',
-        voted: '0',
     },
 
     {
         name: 'usb',
         image: '../usb.gif',
-        voted: '0',
     },
 
     {
         name: 'water-can',
         image: '../water-can.jpg',
-        voted: '0',
     },
 
     {
         name: 'wine-glass',
         image: '../wine-glass.jpg',
-        voted: '0',
     },
 ];
