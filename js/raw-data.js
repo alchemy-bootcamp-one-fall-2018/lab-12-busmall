9d13fa295589d6ea696c6c5995e9ae5ec914261b
Lance Owen Merrill <Lanceom@hawaii.edu>
2018-11-07 17:42:46 -0800

    gave raw data a count

--- js/raw-data.js
+++ js/raw-data.js
@@ -3,31 +3,44 @@ const products = [
     
     {
         name: 'bag',
-        image: '../assets/bag.jpg'
+        image: '../assets/bag.jpg',
+        count: 0
     },
     {
         name: 'banana',
-        image: '../assets/banana.jpg'
+        image: '../assets/banana.jpg',
+        count: 0
+
     },
     {
         name: 'bathroom',
-        image: '../assets/bathroom.jpg'
+        image: '../assets/bathroom.jpg',
+        count: 0
+
     },
     {
         name: 'boots',
-        image: '../assets/boots.jpg'
+        image: '../assets/boots.jpg',
+        count: 0
+
     },
     {
         name: 'breakfast',
-        image: '../assets/breakfast.jpg'
+        image: '../assets/breakfast.jpg',
+        count: 0
+
     },
     {
         name: 'bubblegum',
-        image: '../assets/bubblegum.jpg'
+        image: '../assets/bubblegum.jpg',
+        count: 0
+
     },
     {
         name: 'chair',
-        image: '../assets/chair.jpg'
+        image: '../assets/chair.jpg',
+        count: 0
+
     },
     {
         name: 'cthulhu',
@@ -36,38 +49,54 @@ const products = [
 
     {   
         name:'dog-duck',
-        image: '../assets/dog-duck.jpg'
+        image: '../assets/dog-duck.jpg',
+        count: 0
+
     },
     
     {
         name: 'dragon',
-        image: '../assets/dragon.jpg'
+        image: '../assets/dragon.jpg',
+        count: 0
+
     },
     
     {
         name:'pen',
-        image: '../assets/pen.jpg'
+        image: '../assets/pen.jpg',
+        count: 0
+
     },
     
     {
         name: 'pet-sweep',
-        image: '../assets/pet-sweep.jpg'
+        image: '../assets/pet-sweep.jpg',
+        count: 0
+
     },
     {
         name: 'unicorn-meat',
-        image: '../assets/unicorn.jpg'
+        image: '../assets/unicorn.jpg',
+        count: 0
+
     },
     {
         name: 'usb',
-        image: '../assets/usb.gif'
+        image: '../assets/usb.gif',
+        count: 0
+
     },
     {
         name: 'water-can',
-        image: '../assets/water-can.jpg'
+        image: '../assets/water-can.jpg',
+        count: 0
+
     },
     {
         name: 'wine-glass',
-        image: '../assets/wine-glass.jpg'
+        image: '../assets/wine-glass.jpg',
+        count: 0
+
     }
 
 
