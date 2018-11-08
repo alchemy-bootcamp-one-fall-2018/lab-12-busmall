ef3d9e77959cd021251cbae623bc42e2286a20c0
Shaba9 <shaba92@gmail.com>
2018-11-08 15:53:04 -0800

    changed directory path for images

--- js/product-list.js
+++ js/product-list.js
@@ -1,83 +1,83 @@
 const productList = [
     {
         name: 'Bag',
-        image_url: '../assets/bag.jpg'
+        image_url: '/assets/bag.jpg'
     },
     {
         name: 'Banana',
-        'image_url': '../assets/banana.jpg'
+        'image_url': '/assets/banana.jpg'
     },
     {
         name: 'Bathroom',
-        image_url: '../assets/bathroom.jpg'
+        image_url: '/assets/bathroom.jpg'
     },
     {
         name: 'Boots',
-        image_url: '../assets/boots.jpg'
+        image_url: '/assets/boots.jpg'
     },
     {
         name: 'Breakfast',
-        image_url: '../assets/breakfast.jpg'
+        image_url: '/assets/breakfast.jpg'
     },
     {
         name: 'Bubblegum',
-        image_url: '../assets/bubblegum.jpg'
+        image_url: '/assets/bubblegum.jpg'
     },
     {
         name: 'Chair',
-        image_url: '../assets/chair.jpg'
+        image_url: '/assets/chair.jpg'
     },
     {
         name: 'Cthulhu',
-        image_url: '../assets/cthulhu.jpg'
+        image_url: '/assets/cthulhu.jpg'
     },
     {
         name: 'Dog-Duck',
-        image_url: '../assets/dog-duck.jpg'
+        image_url: '/assets/dog-duck.jpg'
     },
     {
         name: 'Dragon',
-        image_url: '../assets/dragon.jpg'
+        image_url: '/assets/dragon.jpg'
     },
     {
         name: 'Pen',
-        image_url: '../assets/pen.jpg'
+        image_url: '/assets/pen.jpg'
     },
     {
         name: 'Pet-Sweep',
-        image_url: '../assets/pet-sweep.jpg'
+        image_url: '/assets/pet-sweep.jpg'
     },
     {
         name: 'Scissors',
-        image_url: '../assets/scissors.jpg'
+        image_url: '/assets/scissors.jpg'
     },
     {
         name: 'Shark',
-        image_url: '../assets/shark.jpg'
+        image_url: '/assets/shark.jpg'
     },
     {
         name: 'Sweep',
-        image_url: '../assets/sweep.png'
+        image_url: '/assets/sweep.png'
     },
     {
         name: 'Tauntaun',
-        image_url: '../assets/tauntaun.jpg'
+        image_url: '/assets/tauntaun.jpg'
     },
     {
         name: 'Unicorn',
-        image_url: '../assets/unicorn.jpg'
+        image_url: '/assets/unicorn.jpg'
     },
     {
         name: 'USB',
-        image_url: '../assets/usb.gif'
+        image_url: '/assets/usb.gif'
     },
     {
         name: 'Water-Can',
-        image_url: '../assets/water-can.jpg'
+        image_url: '/assets/water-can.jpg'
     },
     {
         name: 'Wine-Glass',
-        image_url: '../assets/wine-glass.jpg'
+        image_url: '/assets/wine-glass.jpg'
     }
 ];
 
