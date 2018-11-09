50a85067ba90ebe6c31a70b57d277dec4ef7be5e
Connor Frendt <cjacksonfx@yahoo.com>
2018-11-08 16:32:10 -0800

    trying to render images

--- js/image.js
+++ js/image.js
@@ -1,15 +1,17 @@
 import html from './html.js';
+import products from './product-list.js';
 
 function makeTemplate() {
     return html`
         <li>This is a list</li>
-        <li>
+        <li><img src="${products.img}>
     `;
 }
 
 export default class Image {
     render() {
         const dom = makeTemplate();
+        console.log(products.img);
         return dom;
     }
 }
