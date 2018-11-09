56fdc6b1d4d220db24646be46557a3d535e0730a
Shaba9 <shaba92@gmail.com>
2018-11-08 17:11:27 -0800

    clicks and views combined in chart

--- js/data-chart.js
+++ js/data-chart.js
@@ -1,15 +1,17 @@
 import surveyApi from './survey-api.js';
 
-function makeTemplate(canvas, info, key) {
+function makeTemplate(canvas, info) {
     const ctx = canvas.getContext('2d');
 
     let labels = [];
-    let data = [];
+    let views = [];
+    let clicks = [];
 
     info.forEach(set => {
         if(!labels.includes(set.name)) {
             labels.push(set.name);
-            data.push(set[key]);
+            views.push(set.views);
+            clicks.push(set.clicks);
         }
     });
 
@@ -18,13 +20,23 @@ function makeTemplate(canvas, info, key) {
         type: 'bar',
         data: {
             labels: labels,
-            datasets: [{
-                label: '# of Views',
-                data: data,
-                backgroundColor: 'rgba(54, 162, 235, 0.2)',
-                borderColor: 'black',                
-                borderWidth: 1
-            }]
+            datasets: [
+                {
+                    label: '# of Views',
+                    data: views,
+                    backgroundColor: 'green',
+                    borderColor: 'black',
+                    borderWidth: 1
+                },
+                {
+                    label: '# of Clicks',
+                    data: clicks,
+                    backgroundColor: 'rgba(54, 162, 235, 0.2)',
+                    borderColor: 'black',
+                    borderWidth: 1
+                },
+                
+            ]
         },
         options: {
             responsive: true,
@@ -42,18 +54,15 @@ function makeTemplate(canvas, info, key) {
 
 class DataChart {
     render() {
-        const canvasViews = document.getElementById('views');
-        const canvasClicks = document.getElementById('clicks');
-        const canvasTotalViews = document.getElementById('totalViews');
-        const canvasTotalClicks = document.getElementById('totalClicks');
+        const canvasViews = document.getElementById('session');
+        const canvasTotalViews = document.getElementById('total');
+
         const session = surveyApi.getSession();
         const allSessions = surveyApi.getDisProds();
 
         if(canvasViews) {
-            makeTemplate(canvasViews, session, 'views');
-            makeTemplate(canvasClicks, session, 'clicks');
-            makeTemplate(canvasTotalViews, allSessions, 'views');
-            makeTemplate(canvasTotalClicks, allSessions, 'clicks');
+            makeTemplate(canvasViews, session);
+            makeTemplate(canvasTotalViews, allSessions);
         }
 
     }
