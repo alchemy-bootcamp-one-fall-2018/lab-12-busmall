f95d445a052793824a7a0b44260b7f86959052da
Abel Quintero <abel.j.quintero@gmail.com>
2018-11-09 17:05:18 -0800

    staged previously unstaged commits

--- JS/image-display.js
+++ JS/image-display.js
@@ -19,26 +19,15 @@ class ImageDisplay {
 
     }
     render() { 
-        // this.getImage();
         const dom = makeTemplate(this.product);
         const listItem = dom.querySelector('.list-item');
         listItem.addEventListener('click', () => {
-            // eslint-disable-next-line no-console
             console.log('product name is ', this.product.name);
-            
         });
-  
-     
-        return dom;
-    }
 
-    // getImage() {
-    //     for(let i = 0; i < 3; i++) {
-    //         const index = Math.floor(Math.random() * 25);
-            // let imi
-    //     }
-    // }
-}
-        
-          
+        return dom;
+  
+    }
+} 
+
 export default ImageDisplay;
