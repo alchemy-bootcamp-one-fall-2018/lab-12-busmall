8a22b9d22d2408893fe98f0be60fe5917a425cac
Teonna Zaragoza <teonna.zaragoza@gmail.com>
2018-11-10 09:40:51 -0800

    showing both views

--- js/reports/view-chart.js
+++ js/reports/view-chart.js
@@ -20,30 +20,42 @@ export default class ViewChart {
         const ctx = canvas.getContext('2d');
         
         let label = [];
-        let data = [];
-        let info = [];
+        let viewCount = [];
+        let clickCount = [];
         
         
         for(let i = 0; i < this.results.length; i++) {
             const card = this.results[i];
            
             label.push(card.name);
-            data.push(card.views);
-            info.push(card.clicks);
+            viewCount.push(card.views);
+            clickCount.push(card.clicks);
         }
 
         this.chart = new Chart(ctx, {
             type: 'bar',
             data: {
                 labels: label,
-                datasets: [{
-                    label: '# of Views',
-                    data: data,
+                datasets: [{ 
+                    label: '# of Views', 
+                    data: viewCount,
+
+                    
                     backgroundColor: 'rgba(54, 162, 235, 0.2)',
                     borderColor: 'rgba(54, 162, 235, 1)',
                     borderWidth: 1
+                },
+                {
+                    label: '# of Clicks',
+                    data: clickCount,
+
+                    backgroundColor: 'rgba(54, 162, 235, 0.2)',
+                    borderColor: 'rgba(54, 162, 235, 1)',
+                    borderWidth: 1
+
                 }]
             },
+
             options: {
                 responsive: true,
                 maintainAspectRatio: false,
