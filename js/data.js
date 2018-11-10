8d0053a78225615cd79837ef302f0956ad2fd0aa
Cheri A Portman <cheriportman@hotmail.com>
2018-11-09 17:03:57 -0800

    added views and votes to data array

--- js/data.js
+++ js/data.js
@@ -3,102 +3,142 @@
 export default [
     {
         name: 'bag',
-        image: '../assets/bag.jpg'
+        image: '../assets/bag.jpg',
+        views: '0',
+        votes: '0',
     },
 
     {
         name: 'banana',
-        image: '../assets/banana.jpg'
+        image: '../assets/banana.jpg',
+        views: '0',
+        votes: '0',
     },
 
     {
         name: 'bathroom',
-        image: '../assets/bathroom.jpg'
+        image: '../assets/bathroom.jpg',
+        views: '0',
+        votes: '0',
     }, 
 
     {
         name: 'boots',
-        image: '../assets/boots.jpg'
+        image: '../assets/boots.jpg',
+        views: '0',
+        votes: '0',
     },
 
     {
         name: 'breakfast',
-        image: '../assets/breakfast.jpg'
+        image: '../assets/breakfast.jpg',
+        views: '0',
+        votes: '0',
     },
     
     {
         name: 'bubblegum',
-        image: '../assets/bubblegum.jpg'
+        image: '../assets/bubblegum.jpg',
+        views: '0',
+        votes: '0',
     },  
     
     {
         name: 'chair',
-        image: '../assets/chair.jpg'
+        image: '../assets/chair.jpg',
+        views: '0',
+        votes: '0',
     },  
     
     {
         name: 'cthulhu',
-        image: '../assets/cthulhu.jpg'
+        image: '../assets/cthulhu.jpg',
+        views: '0',
+        votes: '0',
     },
     
     {
         name: 'dog-duck',
-        image: '../assets/dog-duck.jpg'
+        image: '../assets/dog-duck.jpg',
+        views: '0',
+        votes: '0',
     },
     
     {
         name: 'dragon',
-        image: '../assets/dragon.jpg'
+        image: '../assets/dragon.jpg',
+        views: '0',
+        votes: '0',
     },  
     
     {
         name: 'pen',
-        image: '../assets/pen.jpg'
+        image: '../assets/pen.jpg',
+        views: '0',
+        votes: '0',
     },  
     
     {
         name: 'pet-sweep',
-        image: '../assets/pet-sweep.jpg'
+        image: '../assets/pet-sweep.jpg',
+        views: '0',
+        votes: '0',
     },  
     
     {
         name: 'scissors.jpg',
-        image: '../assets/scissors.jpg'
+        image: '../assets/scissors.jpg',
+        views: '0',
+        votes: '0',
     },  
     
     {
         name: 'shark',
-        image: '../assets/shark.jpg'
+        image: '../assets/shark.jpg',
+        views: '0',
+        votes: '0',
     },
 
     {
         name: 'sweep',
-        image: '../assets/sweep.png'
+        image: '../assets/sweep.png',
+        views: '0',
+        votes: '0',
     },
 
     {
         name: 'tauntaun',
-        image: '../assets/tauntaun.jpg'
+        image: '../assets/tauntaun.jpg',
+        views: '0',
+        votes: '0',
     },
 
     {
         name: 'unicorn',
-        image: '../assets/unicorn.jpg'
+        image: '../assets/unicorn.jpg',
+        views: '0',
+        votes: '0',
     },
 
     {
         name: 'usb',
-        image: '../assets/usb.gif'
+        image: '../assets/usb.gif',
+        views: '0',
+        votes: '0',
     },
 
     {
         name: 'water-can',
-        image: '../assets/water-can.jpg'
+        image: '../assets/water-can.jpg',
+        views: '0',
+        votes: '0',
     },
 
     {
         name: 'wine-glass',
-        image: '../assets/wine-glass.jpg'
+        image: '../assets/wine-glass.jpg',
+        views: '0',
+        votes: '0',
     },
 ];
 
