bdb193789d4e57d78ab08318be4faadec6936d70
Christina T. Herlocker <cherlocker3113@gmail.com>
2018-11-12 01:32:18 -0800

    commented out build-in-progress for report to submit stable app

--- js/report-items/report-app.js
+++ js/report-items/report-app.js
@@ -1,41 +1,41 @@
-import html from '../../html.js';
+// import html from '../../html.js';
 
-function makeTemplate() {
-    return html`
-    <header class="products-section">
-    </header>
-`;
-}
+// function makeTemplate() {
+//     return html`
+//     <header class="products-section">
+//     </header>
+// `;
+// }
 
-class ImageApp {
-    render() {
-        const dom = makeTemplate();
-        const productsSection = dom.querySelector('.products-section');
-        const productSelector = new ProductSelector(products, () => {
-            window.location.href = '/user_summary.html';
-        });
+// class ImageApp {
+//     render() {
+//         const dom = makeTemplate();
+//         const productsSection = dom.querySelector('.products-section');
+//         const productSelector = new ProductSelector(products, () => {
+//             window.location.href = '/user_summary.html';
+//         });
 
-        productsSection.appendChild(productSelector.render());
-        return dom;
-    }
-}
+//         productsSection.appendChild(productSelector.render());
+//         return dom;
+//     }
+// }
 
-const imageApp = new ImageApp();
-document.getElementById('root').appendChild(imageApp.render());
+// const imageApp = new ImageApp();
+// document.getElementById('root').appendChild(imageApp.render());
 
-class reportApp {
-    constructor ctx = {
-        document.getElementById('chart').getContext('2d'),
-    var chart = new Chart(ctx, {
-        type: 'line',
-        data: {
-            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-            datasets: [{
-                label: 'My First dataset',
-                backgroundColor: 'rgb(255, 99, 132)',
-                borderColor: 'rgb(255, 99, 132)',
-                data: [0, 10, 5, 2, 20, 30, 45],
-            }],
-        },
-    }),
-};
+// class reportApp {
+//     thing = {
+//         document.getElementById('chart').getContext('2d'),
+//         var chart = new Chart(thing, {
+//             type: 'line',
+//             data: {
+//                 labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+//                 datasets: [{
+//                     label: 'My First dataset',
+//                     backgroundColor: 'rgb(255, 99, 132)',
+//                     borderColor: 'rgb(255, 99, 132)',
+//                     data: [0, 10, 5, 2, 20, 30, 45],
+//                 }],
+//             },
+//      }),
+// };
--- js/user-summary/user-summary-app.js
+++ js/user-summary/user-summary-app.js
@@ -1,36 +1,37 @@
-import html from '../html.js';
-import productsApi from '../products-api.js';
-import ProductSelector from '../product-selector.js';
+// import html from '../html.js';
+// import productsApi from '../products-api.js';
+// import ProductSelector from '../product-selector.js';
 
-const products = productsApi.getAll();
+// const products = productsApi.getAll();
 
-function makeTemplate() {
-    return html`
-    <section class="summary-table">
-        <h2>
-            Summary of Results
-        </h2>
-        <div class="user-summary">
-        </div>
-    </section>
-`;
-}
+// function makeTemplate() {
+//     return html`
+//     <section class="summary-table">
+//         <h2>
+//             Summary of Results
+//         </h2>
+//         <div class="user-summary">
+//         </div>
+//     </section>
+// `;
+// }
 
-class SummaryApp {
-    render() {
-        const dom = makeTemplate();
-        const dataRow = dom.querySelector('.user-summary');
-        const productz = products;
-        const percent = (clicks/views * 100);
-        const populateRow = new PopulateRow({
-            name: productz.name,
-            views: productz.views,
-            clicks: productz.clicks,
-        });
-        var phrase = 'Item ' + name + 'received ' + clicks + ' clicks, out of ' + views + ' views, totaling ' + percent + 'of total votes.',
-
-        dataRow.appendChild(phrase.render()),
-        return dom,
+// class SummaryApp {
+//     render() {
+//         const dom = makeTemplate();
+//         const dataRow = dom.querySelector('.user-summary');
+//         const productz = products;
+//         const percent = (clicks/views * 100);
+//         const populateRow = new PopulateRow({
+//             name: productz.name,
+//             views: productz.views,
+//             clicks: productz.clicks,
+//             percent = productz.
+//         var phrase = 'Item ' + name + 'received ' + clicks + ' clicks, out of ' + views + ' views, totaling ' + percent + 'of total votes.',
+    
+//         dataRow.appendChild(phrase.render()),
+//         return dom,
+//         });
         
-    },
-};
+//     },
+// };
